3f70fd2af8d4cf95d64075a85ec6da1ce1980d88
Pratik Panchal <ppanchal@adobe.com>
2021-09-22 17:24:58 +0530

    Update release configs

--- .scripts/unrelease.sh
+++ .scripts/unrelease.sh
@@ -6,13 +6,16 @@ PROJECT_NAME=leetcode-tester
 # Check for version parameter
 [ -z "$1" ] && echo 'Please provide version to delete: "./unrelease.sh 0.0.1"' && exit 1
 
+# Clean any release files
+mvn release:clean
+
 # Delete git release tag and branch
-#git tag --delete "$PROJECT_NAME-$1"
-#git push --delete origin "$PROJECT_NAME-$1"
+git tag --delete "$PROJECT_NAME-$1"
+git push --delete origin "$PROJECT_NAME-$1"
 
 # Get package id
 PACKAGE_ID=$(curl "https://api.github.com/user/packages/maven/$PROJECT_GROUP.$PROJECT_NAME/versions" \
-  -H "Authorization: Bearer $GITHUB_PAT_CODI" \
+  -H "Authorization: Bearer $GITHUB_PAT" \
   -H 'Accept: application/vnd.github.v3+json' \
   2>/dev/null | jq -r ".[] | select(.name == \"$1\") | .id")
 
@@ -21,7 +24,7 @@ PACKAGE_ID=$(curl "https://api.github.com/user/packages/maven/$PROJECT_GROUP.$PR
 # Delete package
 
 curl -X DELETE "https://api.github.com/user/packages/maven/$PROJECT_GROUP.$PROJECT_NAME/versions/$PACKAGE_ID" \
-  -H "Authorization: Bearer $GITHUB_PAT_CODI" \
+  -H "Authorization: Bearer $GITHUB_PAT" \
   -H 'Accept: application/vnd.github.v3+json' \
   2>/dev/null
 
